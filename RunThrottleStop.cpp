2bcd0ffa8474d1afe89eaff764e3c6d78f0c6e14
elw773 <45023245+elw773@users.noreply.github.com>
2021-01-27 03:52:08 -0500

    removed junk files

--- RunThrottleStop.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-#include <Windows.h>
-
-int main( )
-{    
-    ShellExecute(NULL, _T("open"), _T("\"C:\\Program Files\\ThrottleStop\\ThrottleStop.exe\""), NULL, NULL, SW_RESTORE);
-    
-    return 0;
-}
